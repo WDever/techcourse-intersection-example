2a1bf3ff54630eeeb6491d5c975ba876a1b41aea
WDever <alsrbtkfkd7@gmail.com>
2020-04-10 18:16:29 +0900

    use intersection observer api

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {
+  useRef,
+  useState,
+  useEffect,
+  RefObject,
+  ReactElement,
+} from "react";
+import "./App.css";
+import { useIntersection } from "./hooks";
 
 function App() {
+  const [list, setList] = useState<ReactElement[]>([
+    <div className="box first">{0}</div>,
+  ]);
+
+  const ref: RefObject<HTMLDivElement> = useRef<HTMLDivElement>(null);
+
+  const intersection = useIntersection(ref, {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0.1,
+  });
+
+  const addList = (): void => {
+    setTimeout(() => {
+      setList(list.concat(<div className="box">{list.length}</div>));
+    }, 2000);
+  };
+
+  useEffect(() => {
+    if (!(intersection && intersection.isIntersecting)) {
+      return;
+    }
+    addList();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [intersection]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="list">
+        {list}
+        <div className="loader" ref={ref}>
+          {intersection?.isIntersecting ? "로딩중" : ""}
+        </div>
+      </div>
     </div>
   );
 }
